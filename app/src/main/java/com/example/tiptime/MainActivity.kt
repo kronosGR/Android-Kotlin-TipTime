4c3836cb9400e3851cbcbbfdd0d8be4b27cdbb67
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-11-27 20:55:54 +0100

    code improvement and avoid null error

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,19 +17,22 @@ class MainActivity : AppCompatActivity() {
         binding.calcualteButton.setOnClickListener { calculateTip() }
     }
 
-    fun calculateTip() {
+    private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResul.text = ""
+            return
+        }
 
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
 
         var tip = tipPercentage * cost
-        var roundUp = binding.roundUpSwitch.isChecked
+        val roundUp = binding.roundUpSwitch.isChecked
         if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
